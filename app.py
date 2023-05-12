3a115d22a94c319e40bda0493c7013b8ba48d90c
big-D-notation <48317291+big-D-notation@users.noreply.github.com>
2023-05-12 20:56:06 +0300

    profile working

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from userdb import UserDB
 
+
 app = Flask(__name__)
 app.secret_key = "secret_key"
+
 
 @app.route("/")
 def home():
     if "username" in session:
         return render_template("index.html", username=session["username"])
     return redirect(url_for("login"))
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -20,10 +23,11 @@ def login():
         user = users_db.get_user(username)
         if user and user[2] == password:
             session["username"] = username
-            return redirect(url_for("home"))
+            return redirect(url_for("profile"))
         else:
             return render_template("login.html", error="Invalid username or password")
     return render_template("login.html")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -38,8 +42,9 @@ def register():
             return render_template("register.html", error="Username already exists")
         users_db.create_user(username, password, email)  # Updated create_user method
         session["username"] = username
-        return redirect(url_for("home"))
+        return redirect(url_for("profile"))
     return render_template("register.html")
+
 
 @app.route("/reset_password", methods=["GET", "POST"])
 def reset_password():
@@ -56,10 +61,71 @@ def reset_password():
             return render_template("reset_password.html", error="Invalid username")
     return render_template("reset_password.html")
 
+
 @app.route("/logout")
 def logout():
     session.pop("username", None)
     return redirect(url_for("login"))
+
+
+@app.route("/add_post", methods=["GET", "POST"])
+def add_post():
+    if "username" in session:
+        users_db = UserDB()
+
+        if request.method == "POST":
+            username = session["username"]
+            title = request.form["title"]
+            content = request.form["content"]
+
+            users_db.add_post(username, title, content)
+            return redirect(url_for("profile"))
+
+        return render_template("add_post.html")
+    return redirect(url_for("login"))
+
+
+@app.route("/add_comment/<int:post_id>", methods=["GET", "POST"])
+def add_comment(post_id):
+    if "username" in session:
+        users_db = UserDB()
+
+        if request.method == "POST":
+            username = session["username"]
+            comment = request.form["comment"]
+
+            users_db.add_comment(post_id, username, comment)
+            return redirect(url_for("profile"))
+
+        return render_template("add_comment.html", post_id=post_id)
+    return redirect(url_for("login"))
+
+
+@app.route("/profile")
+def profile():
+    if "username" in session:
+        username = session["username"]
+        users_db = UserDB()
+
+        user_posts = users_db.get_user_posts(username)
+        print(user_posts)
+
+        return render_template("profile.html", username=username, user_posts=user_posts)
+    return redirect(url_for("login"))
+
+
+@app.route("/profile/<username>")
+def stranger_profile(username):
+    if "username" in session:
+        this_username = session["username"]
+        users_db = UserDB()
+
+        user_posts = users_db.get_user_posts(username)
+        print(user_posts)
+
+        return render_template("stranger_profile.html", this_username=this_username, username=username, user_posts=user_posts)
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81)
--- userdb.py
+++ userdb.py
@@ -19,6 +19,28 @@ class UserDB:
         self.cursor.execute(query)
         self.conn.commit()
 
+        query = """
+        CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            title TEXT NOT NULL,
+            content TEXT NOT NULL
+        )
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
+
+        query = """
+        CREATE TABLE IF NOT EXISTS comments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            post_id INTEGER NOT NULL,
+            username TEXT NOT NULL,
+            comment TEXT NOT NULL
+        )
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
+
     def get_user(self, username):
         query = "SELECT * FROM users WHERE username = ?"
         self.cursor.execute(query, (username,))
@@ -33,4 +55,24 @@ class UserDB:
         query = "UPDATE users SET password = ? WHERE username = ?"
         self.cursor.execute(query, (new_password, username))
         self.conn.commit()
+
+    def add_post(self, username, title, content):
+        query = "INSERT INTO posts (username, title, content) VALUES (?, ?, ?)"
+        self.cursor.execute(query, (username, title, content))
+        self.conn.commit()
+
+    def add_comment(self, post_id, username, comment):
+        query = "INSERT INTO comments (post_id, username, comment) VALUES (?, ?, ?)"
+        self.cursor.execute(query, (post_id, username, comment))
+        self.conn.commit()
+
+    def get_user_posts(self, username):
+        query = "SELECT * FROM posts WHERE username = ?"
+        self.cursor.execute(query, (username,))
+        return self.cursor.fetchall()
+
+    def get_post_comments(self, post_id):
+        query = "SELECT * FROM comments WHERE post_id = ?"
+        self.cursor.execute(query, (post_id,))
+        return self.cursor.fetchall()
         
